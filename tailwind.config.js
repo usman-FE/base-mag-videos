f78cbeda1ffddae97d8a15a1313c76273f8902da
Muhammad Usman <ch.usman564@gmail.com>
2022-10-26 09:13:15 +0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {},
+  plugins: [],
+};
+
+// PRIMARY ORAGE #f79400
+// SECONDARY BLUE #316094
+// SECONDARY BLUE DARK #1e4571
+// TEXT_GRAY #766d66
+// TEXT_GRAY 2 #454545
+// TEXT_BLACK #111111
+// EASE cubic-bezier(.4,0,.2,1)
+// SIDEBAR cubic-bezier(.86,.01,.77,.78)
+// DIRT font-['Russo_One',_sans-serif]
+// PRIMARY font-['Jost',_sans-serif]
+
+/*
+
+bodyOverflow: function (modal) {
+  document.querySelector('body').classList.remove('overflow-hidden', 'overflow-visible');
+  document.querySelector('body').classList.add(modal ? 'overflow-hidden' : 'overflow-visible');
+}
+
+*/
